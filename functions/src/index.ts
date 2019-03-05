3f6021eb9dbc6b315c71c8feb5d61be8cf8a2973
lbilde <ljb@easv.dk>
2019-03-05 14:03:25 +0100

    Adding HTTP Get rquest to list all products

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+admin.initializeApp()
+
+
+
+exports.products = functions.https.onRequest((request, response) => {
+  admin.firestore().collection('products')
+    .get()
+    .then(products => {
+      const listOfProducts: any = [];
+      products.forEach(product => {
+        let prod = product.data();
+        prod.id = product.id;
+        listOfProducts.push(prod);
+      })
+      response.json(listOfProducts);
+    })
+    .catch(err => {console.log(err)})
+
 });
