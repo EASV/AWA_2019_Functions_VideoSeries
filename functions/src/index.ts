3daed36973a684d3c054b109cfe3bedf7fb203c2
lbilde <ljb@easv.dk>
2019-03-08 08:15:48 +0100

    Adding upload Image in functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,31 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp()
 
-
+exports.uploadNewProductImage =
+  functions.storage.object().onFinalize((object) => {
+    return new Promise((resolve, reject) => {
+      if(object && object.name && object.metadata) {
+        console.log('Full uploaded object: ' + JSON.stringify(object))
+        const fileMeta = {
+          lastModified: object.updated,
+          name: object.metadata.originalName,
+          type: 'image/png',
+          size: object.size
+        };
+        console.log('Object Name: ' + object.name);
+        const nameForDoc = object.name.split('/')[1];
+                            // product-pictures/kjsdfjksdkjfs
+        console.log('Object Name for DOC (ID): ' + nameForDoc);
+        admin.firestore().collection('files')
+          .doc(nameForDoc)
+          .set(fileMeta)
+          .then(value => resolve(value))
+          .catch(err => reject(err))
+      } else {
+        reject('Error happened, not enough metadata or file data');
+      }
+    });
+});
 
 exports.products = functions.https.onRequest((request, response) => {
   admin.firestore().collection('products')
--- src/app/files/shared/file.service.ts
+++ src/app/files/shared/file.service.ts
@@ -26,38 +26,21 @@ export class FileService {
   }
 
   upload(file: File): Observable<FileMetadata> {
-    return this.addFileMetadata(
-      {
-        name: file.name,
-        type: file.type,
-        size: file.size,
-        lastModified: file.lastModified
-      }
-    ).pipe(
-      switchMap(fileMeta => {
-        return defer(() =>
-          this.storage.ref('product-pictures/' + fileMeta.id)
-          .put(file)
-          .then()
-        ).pipe(
-          map(fileRef => {
-            return fileMeta;
-          })
-        );
-      })
-    );
-  }
-
-  addFileMetadata(meta: FileMetadata): Observable<FileMetadata> {
+    const uid = this.db.createId();
     return defer(() =>
-      this.db.collection('files')
-      .add(meta)
-    ).pipe(
-      map(documentRef => {
-        meta.id = documentRef.id;
-        return meta;
-      })
-    );
+        this.storage.ref('product-pictures/' + uid)
+        .put(file, {
+          customMetadata: {
+            originalName: file.name
+          }
+        })
+        .then()
+      ).pipe(
+        map(fileRef => {
+          fileRef.id = uid;
+          return fileRef;
+        })
+      );
   }
 
   getFileUrl(id: string): Observable<any> {
