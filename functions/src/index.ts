d216aa91cdad60601a07427a98b3dbb2e456f6ae
lbilde <ljb@easv.dk>
2019-03-12 18:36:45 +0100

    splitting code and adding delete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,43 +1,12 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as deleteProducts from './delete-products-function';
+import * as productRest from './products-rest-endpoint';
+import * as uploadNewProductImage from './upload-new-product-image-function';
 
-admin.initializeApp()
+admin.initializeApp();
 
-exports.uploadNewProductImage =
-  functions.storage.object().onFinalize((object) => {
-    return new Promise((resolve, reject) => {
-      if(object && object.name && object.metadata) {
-        const fileMeta = {
-          lastModified: object.updated,
-          name: object.metadata.originalName,
-          type: 'image/png',
-          size: object.size
-        };
-        const nameForDoc = object.name.split('/')[1];
-                            // product-pictures/kjsdfjksdkjfs
-        admin.firestore().collection('files')
-          .doc(nameForDoc)
-          .set(fileMeta)
-          .then(value => resolve(value))
-          .catch(err => reject(err))
-      } else {
-        reject('Error happened, not enough metadata or file data');
-      }
-    });
-});
-
-exports.products = functions.https.onRequest((request, response) => {
-  admin.firestore().collection('products')
-    .get()
-    .then(products => {
-      const listOfProducts: any = [];
-      products.forEach(product => {
-        let prod = product.data();
-        prod.id = product.id;
-        listOfProducts.push(prod);
-      })
-      response.json(listOfProducts);
-    })
-    .catch(err => {console.log(err)})
-
-});
+module.exports = {
+  ...deleteProducts,
+  ...productRest,
+  ...uploadNewProductImage
+}
---
+++ functions/src/products-rest-endpoint.ts
@@ -0,0 +1,19 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+exports.products = functions.https.onRequest((request, response) => {
+  admin.firestore().collection('products')
+    .get()
+    .then(products => {
+      const listOfProducts: any = [];
+      products.forEach(product => {
+        let prod = product.data();
+        prod.id = product.id;
+        listOfProducts.push(prod);
+      })
+      response.json(listOfProducts);
+    })
+    .catch(err => {console.log(err)})
+
+});
+
